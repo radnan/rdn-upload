78f5ab28bb8cafbb96c010ad9af85127677cd365
radnan <rafiadnan47@gmail.com>
2013-12-24 20:43:32 -0600

    use parent locator to get upload container

--- src/RdnUpload/Factory/Controller/Plugin/Uploads.php
+++ src/RdnUpload/Factory/Controller/Plugin/Uploads.php
@@ -5,12 +5,22 @@ namespace RdnUpload\Factory\Controller\Plugin;
 use RdnUpload\ContainerInterface;
 use RdnUpload\Controller\Plugin;
 use Zend\ServiceManager\FactoryInterface;
+use Zend\ServiceManager\ServiceLocatorAwareInterface;
 use Zend\ServiceManager\ServiceLocatorInterface;
 
 class Uploads implements FactoryInterface
 {
-	public function createService(ServiceLocatorInterface $services)
+	public function createService(ServiceLocatorInterface $plugins)
 	{
+		if ($plugins instanceof ServiceLocatorAwareInterface)
+		{
+			$services = $plugins->getServiceLocator();
+		}
+		else
+		{
+			$services = $plugins;
+		}
+
 		/** @var ContainerInterface $uploads */
 		$uploads = $services->get('RdnUpload\Container');
 		return new Plugin\Uploads($uploads);
--- src/RdnUpload/Factory/View/Helper/Uploads.php
+++ src/RdnUpload/Factory/View/Helper/Uploads.php
@@ -5,12 +5,22 @@ namespace RdnUpload\Factory\View\Helper;
 use RdnUpload\ContainerInterface;
 use RdnUpload\View\Helper;
 use Zend\ServiceManager\FactoryInterface;
+use Zend\ServiceManager\ServiceLocatorAwareInterface;
 use Zend\ServiceManager\ServiceLocatorInterface;
 
 class Uploads implements FactoryInterface
 {
-	public function createService(ServiceLocatorInterface $services)
+	public function createService(ServiceLocatorInterface $helpers)
 	{
+		if ($helpers instanceof ServiceLocatorAwareInterface)
+		{
+			$services = $helpers->getServiceLocator();
+		}
+		else
+		{
+			$services = $helpers;
+		}
+
 		/** @var ContainerInterface $uploads */
 		$uploads = $services->get('RdnUpload\Container');
 		return new Helper\Uploads($uploads);
